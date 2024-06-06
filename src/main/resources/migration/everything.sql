ec9a11544c9d99e3d50542ddbbef1a6518645bbb
siimkk <siimkkoger@gmail.com>
2024-06-06 17:11:30 +0300

    Add mock data to db

--- src/main/resources/migration/everything.sql
+++ src/main/resources/migration/everything.sql
@@ -38,3 +38,123 @@ CREATE TABLE public.movies_categories
     FOREIGN KEY (movie_id) REFERENCES public.movies (eidrCode),
     FOREIGN KEY (category_id) REFERENCES public.categories (id)
 );
+
+-- Insert categories
+INSERT INTO public.categories (name)
+VALUES ('Action'),
+       ('Comedy'),
+       ('Drama'),
+       ('Horror'),
+       ('Sci-Fi');
+
+
+-- Insert movies
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/1A2B-3C4D-5E6F-7G8H-9I0J-A', 'Movie One', 7, '2020-01-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/2B3C-4D5E-6F7G-8H9I-0J1K-B', 'Movie Two', 8, '2019-02-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/3C4D-5E6F-7G8H-9I0J-1K2L-C', 'Movie Three', 9, '2018-03-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/4D5E-6F7G-8H9I-0J1K-2L3M-D', 'Movie Four', 6, '2021-04-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/5E6F-7G8H-9I0J-1K2L-3M4N-E', 'Movie Five', 7, '2017-05-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/6F7G-8H9I-0J1K-2L3M-4N5O-F', 'Movie Six', 8, '2022-06-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/7G8H-9I0J-1K2L-3M4N-5O6P-G', 'Movie Seven', 9, '2016-07-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/8H9I-0J1K-2L3M-4N5O-6P7Q-H', 'Movie Eight', 6, '2015-08-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/9I0J-1K2L-3M4N-5O6P-7Q8R-I', 'Movie Nine', 7, '2014-09-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/0J1K-2L3M-4N5O-6P7Q-8R9S-J', 'Movie Ten', 8, '2013-10-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/1K2L-3M4N-5O6P-7Q8R-9S0T-K', 'Movie Eleven', 9, '2012-11-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/2L3M-4N5O-6P7Q-8R9S-0T1U-L', 'Movie Twelve', 6, '2011-12-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/3M4N-5O6P-7Q8R-9S0T-1U2V-M', 'Movie Thirteen', 7, '2010-01-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/4N5O-6P7Q-8R9S-0T1U-2V3W-N', 'Movie Fourteen', 8, '2009-02-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/5O6P-7Q8R-9S0T-1U2V-3W4X-O', 'Movie Fifteen', 9, '2008-03-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/6P7Q-8R9S-0T1U-2V3W-4X5Y-P', 'Movie Sixteen', 6, '2007-04-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/7Q8R-9S0T-1U2V-3W4X-5Y6Z-Q', 'Movie Seventeen', 7, '2006-05-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/8R9S-0T1U-2V3W-4X5Y-6Z7A-R', 'Movie Eighteen', 8, '2005-06-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/9S0T-1U2V-3W4X-5Y6Z-7A8B-S', 'Movie Nineteen', 9, '2004-07-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/0T1U-2V3W-4X5Y-6Z7A-8B9C-T', 'Movie Twenty', 6, '2003-08-01', current_timestamp, current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/1U2V-3W4X-5Y6Z-7A8B-9C0D-U', 'Movie Twenty-One', 7, '2002-09-01', current_timestamp,
+        current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/2V3W-4X5Y-6Z7A-8B9C-0D1E-V', 'Movie Twenty-Two', 8, '2001-10-01', current_timestamp,
+        current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/3W4X-5Y6Z-7A8B-9C0D-1E2F-W', 'Movie Twenty-Three', 9, '2000-11-01', current_timestamp,
+        current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/4X5Y-6Z7A-8B9C-0D1E-2F3G-X', 'Movie Twenty-Four', 6, '1999-12-01', current_timestamp,
+        current_timestamp);
+INSERT INTO public.movies (eidrCode, name, rating, year, created_at, updated_at)
+VALUES ('10.5240/5Y6Z-7A8B-9C0D-1E2F-3G4H-Y', 'Movie Twenty-Five', 7, '1998-01-01', current_timestamp,
+        current_timestamp);
+
+
+-- Insert movie-category associations
+INSERT INTO public.movies_categories (movie_id, category_id)
+VALUES ('10.5240/1A2B-3C4D-5E6F-7G8H-9I0J-A', 1),
+       ('10.5240/1A2B-3C4D-5E6F-7G8H-9I0J-A', 2),
+       ('10.5240/2B3C-4D5E-6F7G-8H9I-0J1K-B', 1),
+       ('10.5240/2B3C-4D5E-6F7G-8H9I-0J1K-B', 3),
+       ('10.5240/3C4D-5E6F-7G8H-9I0J-1K2L-C', 1),
+       ('10.5240/3C4D-5E6F-7G8H-9I0J-1K2L-C', 4),
+       ('10.5240/4D5E-6F7G-8H9I-0J1K-2L3M-D', 2),
+       ('10.5240/4D5E-6F7G-8H9I-0J1K-2L3M-D', 3),
+       ('10.5240/5E6F-7G8H-9I0J-1K2L-3M4N-E', 2),
+       ('10.5240/5E6F-7G8H-9I0J-1K2L-3M4N-E', 5),
+       ('10.5240/6F7G-8H9I-0J1K-2L3M-4N5O-F', 3),
+       ('10.5240/6F7G-8H9I-0J1K-2L3M-4N5O-F', 4),
+       ('10.5240/7G8H-9I0J-1K2L-3M4N-5O6P-G', 3),
+       ('10.5240/7G8H-9I0J-1K2L-3M4N-5O6P-G', 5),
+       ('10.5240/8H9I-0J1K-2L3M-4N5O-6P7Q-H', 4),
+       ('10.5240/8H9I-0J1K-2L3M-4N5O-6P7Q-H', 1),
+       ('10.5240/9I0J-1K2L-3M4N-5O6P-7Q8R-I', 4),
+       ('10.5240/9I0J-1K2L-3M4N-5O6P-7Q8R-I', 2),
+       ('10.5240/0J1K-2L3M-4N5O-6P7Q-8R9S-J', 5),
+       ('10.5240/0J1K-2L3M-4N5O-6P7Q-8R9S-J', 3),
+       ('10.5240/1K2L-3M4N-5O6P-7Q8R-9S0T-K', 5),
+       ('10.5240/1K2L-3M4N-5O6P-7Q8R-9S0T-K', 4),
+       ('10.5240/2L3M-4N5O-6P7Q-8R9S-0T1U-L', 1),
+       ('10.5240/2L3M-4N5O-6P7Q-8R9S-0T1U-L', 5),
+       ('10.5240/3M4N-5O6P-7Q8R-9S0T-1U2V-M', 2),
+       ('10.5240/3M4N-5O6P-7Q8R-9S0T-1U2V-M', 1),
+       ('10.5240/4N5O-6P7Q-8R9S-0T1U-2V3W-N', 2),
+       ('10.5240/4N5O-6P7Q-8R9S-0T1U-2V3W-N', 3),
+       ('10.5240/5O6P-7Q8R-9S0T-1U2V-3W4X-O', 3),
+       ('10.5240/5O6P-7Q8R-9S0T-1U2V-3W4X-O', 1),
+       ('10.5240/6P7Q-8R9S-0T1U-2V3W-4X5Y-P', 3),
+       ('10.5240/6P7Q-8R9S-0T1U-2V3W-4X5Y-P', 5),
+       ('10.5240/7Q8R-9S0T-1U2V-3W4X-5Y6Z-Q', 4),
+       ('10.5240/7Q8R-9S0T-1U2V-3W4X-5Y6Z-Q', 2),
+       ('10.5240/8R9S-0T1U-2V3W-4X5Y-6Z7A-R', 4),
+       ('10.5240/8R9S-0T1U-2V3W-4X5Y-6Z7A-R', 1),
+       ('10.5240/9S0T-1U2V-3W4X-5Y6Z-7A8B-S', 5),
+       ('10.5240/9S0T-1U2V-3W4X-5Y6Z-7A8B-S', 3),
+       ('10.5240/0T1U-2V3W-4X5Y-6Z7A-8B9C-T', 5),
+       ('10.5240/0T1U-2V3W-4X5Y-6Z7A-8B9C-T', 4),
+       ('10.5240/1U2V-3W4X-5Y6Z-7A8B-9C0D-U', 1),
+       ('10.5240/1U2V-3W4X-5Y6Z-7A8B-9C0D-U', 5),
+       ('10.5240/2V3W-4X5Y-6Z7A-8B9C-0D1E-V', 2),
+       ('10.5240/2V3W-4X5Y-6Z7A-8B9C-0D1E-V', 1),
+       ('10.5240/3W4X-5Y6Z-7A8B-9C0D-1E2F-W', 2),
+       ('10.5240/3W4X-5Y6Z-7A8B-9C0D-1E2F-W', 3),
+       ('10.5240/4X5Y-6Z7A-8B9C-0D1E-2F3G-X', 3),
+       ('10.5240/4X5Y-6Z7A-8B9C-0D1E-2F3G-X', 4),
+       ('10.5240/5Y6Z-7A8B-9C0D-1E2F-3G4H-Y', 4),
+       ('10.5240/5Y6Z-7A8B-9C0D-1E2F-3G4H-Y', 5);
