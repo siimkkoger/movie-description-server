d37a50a9ae9355bf7ff0c0841d9f11de1a2bee38
siimkk <siimkkoger@gmail.com>
2024-06-06 13:10:31 +0300

    Initial commit

---
+++ src/main/java/com/example/moviedescriptionsserver/MovieDescriptionsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviedescriptionsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDescriptionsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieDescriptionsServerApplication.class, args);
+    }
+
+}
