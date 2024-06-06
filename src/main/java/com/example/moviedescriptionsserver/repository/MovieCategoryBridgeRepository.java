ef669befdf83d184fcdb1d4cf6594e01c0664bc2
siimkk <siimkkoger@gmail.com>
2024-06-06 16:39:27 +0300

    Implement updating movies

--- src/main/java/com/example/moviedescriptionsserver/repository/MovieCategoryBridgeRepository.java
+++ src/main/java/com/example/moviedescriptionsserver/repository/MovieCategoryBridgeRepository.java
@@ -3,9 +3,15 @@ package com.example.moviedescriptionsserver.repository;
 import com.example.moviedescriptionsserver.entity.MovieCategoryEntity;
 import com.example.moviedescriptionsserver.entity.MovieCategoryEntityId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieCategoryBridgeRepository extends JpaRepository<MovieCategoryEntity, MovieCategoryEntityId> {
+
+    @Query("SELECT mce FROM MovieCategoryEntity mce WHERE mce.id.movieEidr = :eidrCode")
+    List<MovieCategoryEntity> findAllByMovieEidrCode(String eidrCode);
 
 }
--- src/main/java/com/example/moviedescriptionsserver/service/MovieService.java
+++ src/main/java/com/example/moviedescriptionsserver/service/MovieService.java
@@ -60,7 +60,7 @@ public class MovieService {
         movie.setEidrCode(createMovieRequest.eidrCode());
         movie.setName(createMovieRequest.name());
         movie.setRating(createMovieRequest.rating());
-        movie.setYear(createMovieRequest.year());
+        movie.setYear(createMovieRequest.year()); // TODO - aasta ei voi olla tulevikus
         movie.setStatus(createMovieRequest.status());
         MovieEntity savedMovieEntity = movieRepository.save(movie);
 
@@ -78,8 +78,57 @@ public class MovieService {
         return convertToMovieResponse(savedMovieEntity, categoryEntities);
     }
 
+    @Transactional(rollbackFor = Exception.class, isolation = Isolation.SERIALIZABLE)
     public GetMovieResponse updateMovie(UpdateMovieRequest updateMovieRequest) {
-        return null;
+        MovieEntity movieEntity = movieRepository.findByEidrCode(updateMovieRequest.eidrCode());
+        if (movieEntity == null) {
+            throw new IllegalArgumentException("Movie with eidrCode " + updateMovieRequest.eidrCode() + " does not exist.");
+        }
+
+        List<CategoryEntity> categoryEntities = categoryRepository.findCategoriesByIds(updateMovieRequest.categories());
+        if (categoryEntities.size() != updateMovieRequest.categories().size()) {
+            throw new IllegalArgumentException("Some categories do not exist.");
+        }
+
+        // Update movie (if needed)
+        movieEntity.setName(updateMovieRequest.name());
+        movieEntity.setRating(updateMovieRequest.rating());
+        movieEntity.setYear(updateMovieRequest.year());
+        movieEntity.setStatus(updateMovieRequest.status());
+        if (!movieDtoFieldsEqualEntityFields(movieEntity, updateMovieRequest)) {
+            movieRepository.save(movieEntity);
+        }
+
+        // Update categories of that movie (if needed)
+        List<CategoryEntity> currentCategories = categoryRepository.findCategoriesByMovieEidrCode(updateMovieRequest.eidrCode());
+        if (!currentCategoriesEqualRequestedCategories(currentCategories, updateMovieRequest.categories())) {
+            // Remove everything from the bridge table and add the new ones
+            movieCategoryBridgeRepository.deleteAll(movieCategoryBridgeRepository.findAllByMovieEidrCode(updateMovieRequest.eidrCode()));
+            movieCategoryBridgeRepository.saveAll(
+                    categoryEntities.stream()
+                            .map(categoryEntity -> {
+                                var movieCategoryEntity = new MovieCategoryEntity();
+                                movieCategoryEntity.getId().setMovieEidr(updateMovieRequest.eidrCode());
+                                movieCategoryEntity.getId().setCategoryId(categoryEntity.getId());
+                                return movieCategoryEntity;
+                            })
+                            .toList()
+            );
+        }
+
+        return convertToMovieResponse(movieEntity, categoryEntities);
+    }
+
+    private boolean movieDtoFieldsEqualEntityFields(MovieEntity movieEntity, UpdateMovieRequest updateMovieRequest) {
+        return movieEntity.getEidrCode().equals(updateMovieRequest.eidrCode()) &&
+                movieEntity.getName().equals(updateMovieRequest.name()) &&
+                movieEntity.getRating().equals(updateMovieRequest.rating()) &&
+                movieEntity.getYear().equals(updateMovieRequest.year()) &&
+                movieEntity.getStatus().equals(updateMovieRequest.status());
+    }
+
+    private boolean currentCategoriesEqualRequestedCategories(List<CategoryEntity> currentCategories, List<Long> requestedCategories) {
+        return currentCategories.stream().map(CategoryEntity::getId).toList().equals(requestedCategories);
     }
 
     private GetMovieResponse convertToMovieResponse(MovieEntity movieEntity, List<CategoryEntity> categoryEntities) {
